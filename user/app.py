a0a5b078a1cad728ad87daa5dd7f4b65c18657a2
luiz-felipe-b <lfbalaminute@hotmail.com>
2024-09-27 22:36:31 -0300

    first commit
    feat: user microsservices
    
    functions: get users, create users, user login

---
+++ user/app.py
@@ -0,0 +1,49 @@
+from flask import Flask
+from flask.sessions import SecureCookieSessionInterface
+from flask_migrate import Migrate
+from flask_login import LoginManager
+from routes import user_bp
+from dotenv import load_dotenv
+import os
+import models
+
+load_dotenv()
+
+file_path = os.path.abspath(os.getcwd())+"\\database\\user.db"
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + file_path
+
+models.init_app(app)
+app.register_blueprint(user_bp)
+loginManager = LoginManager(app)
+migrate = Migrate(app, models.db)
+
+@loginManager.user_loader
+def load_user(user_id):
+    return models.User.query.filter_by(id=user_id).first()
+
+@loginManager.request_loader
+def load_user_from_request(request):
+    api_key = request.headers.get('Authorization')
+    if api_key:
+        api_key = api_key.replace('Basic ', '', 1)
+        user = models.User.query.filter_by(api_key=api_key).first()
+        if user:
+            return user
+
+    return None
+
+class CustomSessionInterface(SecureCookieSessionInterface):
+    """Prevent creating session from API requests."""
+
+    def save_session(self, *args, **kwargs):
+        if g.get('login_via_header'):
+            return
+        return super(CustomSessionInterface, self).save_session(*args, **kwargs)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ user/routes.py
@@ -0,0 +1,68 @@
+from flask import Blueprint, jsonify, make_response, request
+from flask_login import login_user
+from models import db, User
+from werkzeug.security import generate_password_hash, check_password_hash
+
+user_bp = Blueprint('user', __name__, url_prefix='/user')
+
+@user_bp.route('/all', methods=['GET'])
+def get_all_users():
+    all_user = User.query.all()
+    result = [user.serialize() for user in all_user]
+    response = {
+        'message': 'Returning all users',
+        'result': result
+    }
+    return jsonify(response), 200
+
+@user_bp.route('/create', methods=['POST'])
+def create_user():
+    try:
+        user = User()
+        user.username = request.form["username"]
+        user.password = generate_password_hash(request.form["password"], method='pbkdf2:sha256')
+        user.is_admin = True
+
+        db.session.add(user)
+        db.session.commit()
+        response = {
+            'message': 'Success',
+            'result': user.serialize()
+        }
+        code = 201
+    except Exception as e:
+        print(str(e))
+        response = {
+            'message': 'Error in creating response',
+        }
+        code = 409
+    return jsonify(response), code
+
+@user_bp.route('/login', methods=['POST'])
+def login():
+    username = request.form["username"]
+    password = request.form["password"]
+    user = User.query.filter_by(username=username).first()
+    if not user:
+        code = 401
+        return jsonify({'message': 'Invalid username or password'}), code
+    if check_password_hash(user.password, password):
+        user.update_api_key()
+        db.session.commit()
+        login_user(user)
+        response = {
+            'message': 'Logged in',
+            'api_key': user.api_key,
+            'result': user.serialize()
+        }
+        code = 200
+        return make_response(jsonify(response), code)
+    response = { 'message': 'Access denied'}
+    return make_response(jsonify(response), code)
+
+@user_bp.route('/logout', methods=['POST'])
+def logout():
+    response = {
+        'message': 'Logged out'
+    }
+    return jsonify(response), 200
